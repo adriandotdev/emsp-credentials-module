9e5ebae06c7b5d84634ce3c4252b2976fb9d5b6b
Adrian <marcelonads@gmail.com>
2024-02-05 12:47:57 +0800

    refactor: ADDED parameters for class Crypto's methods
    These added parameters is for dynamic key, and IV when encrypting text.
    These will be used for Token A, and Token C.

--- utils/Crypto.js
+++ utils/Crypto.js
@@ -5,15 +5,15 @@ const iv = process.env.CRYPTO_IV;
 const key = process.env.CRYPTO_SECRET_KEY;
 
 module.exports = class Crypto {
-	static Encrypt(text) {
-		const cipher = crypto.createCipheriv(algorithm, key, iv);
+	static Encrypt(text, dynamicKey, dynamicIV) {
+		const cipher = crypto.createCipheriv(algorithm, dynamicKey, dynamicIV);
 		let encrypted = cipher.update(text, "utf-8", "base64");
 		encrypted += cipher.final("base64");
 		return encrypted;
 	}
 
-	static Decrypt(hash) {
-		const decipher = crypto.createDecipheriv(algorithm, key, iv);
+	static Decrypt(hash, dynamicKey, dynamicIV) {
+		const decipher = crypto.createDecipheriv(algorithm, dynamicKey, dynamicIV);
 		let decryptedData = decipher.update(hash, "base64", "utf-8");
 		decryptedData += decipher.final("utf-8");
 		return decryptedData;
